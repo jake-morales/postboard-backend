e8f986df8f89dcf48fd4562d443af390571a1a77
Jake Morales <jmora060@ucr.edu>
2019-08-23 23:29:25 -0700

    added /search for queries

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,23 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/search').get((req, res) =>{
+    const queryTerm = req.query.q;
+    const queryRegEx = new RegExp(queryTerm, 'i')
+    var query = Post.find({
+        $or: [{title:  queryRegEx  },
+            {description: queryRegEx }]
+    })
+
+    query.exec(function(err, post){
+        if(err){ res.status(404).json(err)}
+        else{
+            res.json(post)
+        }
+    })
+}
+)
+
 router.route('/add').post((req, res) => {
     const title = req.body.title;
     const description = req.body.description;
